be3eb25db5e415d33f1c0df8655557b0d7576e34
Davut Uzun <duzun@k1m39s03.42istanbul.com.tr>
2022-10-16 14:29:14 +0300

    Update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,22 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 21:50:22 by duzun             #+#    #+#             */
-/*   Updated: 2022/10/16 13:35:26 by duzun            ###   ########.fr       */
+/*   Updated: 2022/10/16 14:28:35 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 // Verilen *str stringi tarar space ve özel karakterleri geçer, tek eksi görme
 // durumunda sayı negatif olarak takip edip eden sayıyı tespit eder.
-// char verilen sayiları int bir değere ddönüştürür.
+// char verilen sayiları int bir değere dönüştürür.
 
 int	ft_atoi(const char *str)
 {
-	int			i;
-	int			sign;
-	long int	num;
+	int	i;
+	int	sign;
+	int	num;
 
 	i = 0;
 	sign = 1;
@@ -38,3 +39,18 @@ int	ft_atoi(const char *str)
 	}
 	return (num * sign);
 }
+/*
+int	main(int ac, char **av)
+{
+	int	num;
+
+	if (ac == 2)
+	{
+		// arguman olarak şunları deneyebilirsiniz:
+		// +123, -123, 123, 918273645, -2147483648, 2147483647, abc123, 123abc
+		num = ft_atoi(av[1]);
+		printf("argumanın integer değeri : %d\n", num);
+	}
+	return (0);
+}
+*/

3ea732f5b2ae7fd09e2b6b3c18cbc5890a7b5561
Davut Uzun <duzun@k1m38s01.42istanbul.com.tr>
2022-10-17 14:29:03 +0300

    Update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,28 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:27:05 by duzun             #+#    #+#             */
-/*   Updated: 2022/08/31 19:08:58 by duzun            ###   ########.fr       */
+/*   Updated: 2022/10/17 14:28:08 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //#include <stdio.h>
+//#include <stdlib.h>
 
 // count ile belirtilen dizgiler icin size ile belirtilen boyut kadar hafızada
-// yer tahsis edilir. Bu hafıza alanı 0 ile doldurulur.
+// yer tahsis edilir. Bu hafıza alanı \0 ile doldurulur.
 // geriye bu alanın icerigi dondurulur.
+
+//void	ft_bzero(void *s, size_t n)
+//{
+//	size_t	i;
+
+//	if (n == 0)
+//		return ;
+//	i = 0;
+//	while (i < n)
+//		*((unsigned char *)s + i++) = '\0';
+//}
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -28,15 +40,13 @@ void	*ft_calloc(size_t count, size_t size)
 	return (ptr);
 }
 
-/*
-int	main(void)
-{
-	char	*c = ft_calloc(2, 1);
-	char	x[3] = {'a', 'b', 'c'};
+//int	main(void)
+//{
+//	char	*c = ft_calloc(2, 1);
+//	char	x[4] = {'a', 'b', 'c', '\0'};
 
-	printf("%s\n", c);
-	c = x;
-	printf("%s\n", c);
-	return (0);
-}
-*/
+//	printf("2 bay yer açıldı içi '\\0' ile doldurulur (değer yok): %s\n", c);
+//	c = x;
+//	printf("c işaret etiği adres x işaretçisi ile değiştirildi. %s\n", c);
+//	return (0);
+//}

8e36a8e88d521d246c8ead6c5ec3448ff3a7579d
Davut Uzun <duzun@k1m30s01.42istanbul.com.tr>
2022-10-18 13:07:27 +0300

    Update

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,14 +6,15 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 23:30:28 by duzun             #+#    #+#             */
-/*   Updated: 2022/08/31 19:09:12 by duzun            ###   ########.fr       */
+/*   Updated: 2022/10/18 13:07:06 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+// #include <stdio.h>
+
 // Fonksiyona c değişkeni ile gönderilen değer 0 il1 127 arasında bir değer ise
 // 1 değilse 0 döndürür.
-// #include <stdio.h>
 
 int	ft_isascii(int c)
 {

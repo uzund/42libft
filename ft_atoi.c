1c064d839ab61a6e3803734b69905310cb5b16ae
Davut UZUN <duzun@Davut-iMac.local>
2022-12-11 17:21:21 +0300

    Update...

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,17 @@
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 21:50:22 by duzun             #+#    #+#             */
-/*   Updated: 2022/11/25 23:01:14 by duzun            ###   ########.fr       */
+/*   Updated: 2022/12/11 17:18:31 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-// Verilen *str stringi tarar space ve özel karakterleri geçer, tek eksi görme
-// durumunda sayı negatif olarak takip edip eden sayıyı tespit eder.
-// char verilen sayiları int bir değere dönüştürür.
+// Verilen *str stringi tarar space ve özel karakterleri geçer, tek eksi (-) 
+// görme durumunda negatif olarak (+) yada işaretsiz rakamları pozitif takip
+// edip sayıyı tespit eder. char verilen sayiları int bir değere dönüştürür.
 
 int	ft_atoi(const char *str)
 {
@@ -28,10 +29,9 @@ int	ft_atoi(const char *str)
 	num = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
-	if (str[i] == '-')
-		sign = -1;
 	if (str[i] == '-' || str[i] == '+')
-		i++;
+		if (str[i++] == '-')
+			sign = -1;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		num = (str[i] - '0') + (num * 10);
@@ -49,7 +49,8 @@ int	main(int ac, char **av)
 		// arguman olarak şunları deneyebilirsiniz:
 		// +123, -123, 123, 918273645, -2147483648, 2147483647, abc123, 123abc
 		num = ft_atoi(av[1]);
-		printf("argumanın integer değeri : %d\n", num);
+		pprintf("arguman integer değeri : %d\n", num);
+		printf("orjinal değer : %d\n", atoi(av[1]));
 	}
 	return (0);
 }
